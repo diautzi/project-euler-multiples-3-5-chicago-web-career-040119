dd5780e76b95265ec52f883bd2d4ded7d7065594
Diana Utan <didiutzi@gmail.com>
2021-04-21 21:01:09 +0000

    Automatically backed up by Learn

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,12 @@
 # Enter your procedural solution here!
+def sum_multiples(num) 
+  sum = 0
+  if (num % 3 === true) || (num % 5 === true) 
+    puts num
+    sum += num
+  end
+  sum
+end 
+
+# puts sum_multiples(100)
+
